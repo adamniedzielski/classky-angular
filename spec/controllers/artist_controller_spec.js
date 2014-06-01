18ad617eff953091a6a54d419fbb487fa2c8315c
Adam Niedzielski <adamsunday@gmail.com>
2014-06-01 11:50:22 +0200

    Create unit test for ArtistController

---
+++ spec/controllers/artist_controller_spec.js
@@ -0,0 +1,40 @@
+describe("ArtistController", function() {
+
+  var artistId = 'f5cb2b4f-bfba-4bfc-9d70-82d138829717';
+  var artist = {
+    name: 'AC/DC',
+    albums: [
+      'Back in Black',
+      'High Voltage'
+    ]
+  }
+
+  var deferredResponse;
+  var scope;
+  var service;
+
+  beforeEach(function() {
+    module("app");
+  });
+
+  beforeEach(inject(function($controller, $rootScope, $q, MusicBrainzService) {
+    scope = $rootScope.$new();
+    service = MusicBrainzService;
+
+    deferredResponse = $q.defer();
+    spyOn(service, 'getArtist').andReturn(deferredResponse.promise);
+
+    $controller('ArtistController', {
+      $scope: scope,
+      $routeParams: { id: artistId },
+      MusicBrainzService: service
+    });
+  }));
+
+  it("should get artist from MusicBrainzService", function() {
+    expect(service.getArtist).toHaveBeenCalledWith(artistId);
+    deferredResponse.resolve(artist);
+    scope.$apply();
+    expect(scope.artist).toEqual(artist);
+  });
+});

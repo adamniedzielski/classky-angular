6d135b823e6aa6a4a3f9b8ff461c8b87cb9b06ca
Adam Niedzielski <adamsunday@gmail.com>
2014-06-01 16:11:31 +0200

    Refactor MusicBrainzService

--- app/js/services/music_brainz_service.js
+++ app/js/services/music_brainz_service.js
@@ -1,25 +1,33 @@
 angular.module("app").factory("MusicBrainzService", function ($q, $http) {
 
-  var getArtist = function (id) {
+  var BASE_URL = 'http://musicbrainz.org/ws/2/artist/'
+
+  function getArtist(id) {
     var deferred = $q.defer();
 
-    $http.get('http://musicbrainz.org/ws/2/artist/' + id + '?inc=release-groups&fmt=json')
+    var params = {
+      inc: 'release-groups',
+      fmt: 'json'
+    };
+
+    $http.get(BASE_URL + id, { params: params })
       .success(function (data) {
-
-        var albums = _.map(data['release-groups'], function (album) {
-          return album.title;
-        });
-
-        var result = {
-          name: data.name,
-          albums: albums
-        };
-
-        deferred.resolve(result);
+        deferred.resolve(processResponse(data));
       });
 
     return deferred.promise;
-  };
+  }
+
+  function processResponse(data) {
+    var albums = _.map(data['release-groups'], function (album) {
+      return album.title;
+    });
+
+    return {
+      name: data.name,
+      albums: albums
+    };
+  }
 
   return { getArtist: getArtist };
 });

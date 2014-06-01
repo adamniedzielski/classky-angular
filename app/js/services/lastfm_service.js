9634c00a92f91cdbe3d97647ef75e2c1c8d8e8ad
Adam Niedzielski <adamsunday@gmail.com>
2014-06-01 14:08:30 +0200

    Refactor LastFMService#getHypedArtists: split into smaller methods, extract constants

--- app/js/services/lastfm_service.js
+++ app/js/services/lastfm_service.js
@@ -1,29 +1,43 @@
 angular.module("app").factory("LastFMService", function ($q, $http) {
 
-  var getHypedArtists = function () {
+  var BASE_URL = 'http://ws.audioscrobbler.com/2.0';
+  var API_KEY = 'cca4c83aff122fd01e10eb20e0cb8cf6';
+
+  function getHypedArtists() {
     var deferred = $q.defer();
 
-    $http.get('http://ws.audioscrobbler.com/2.0/?method=chart.gethypedartists&api_key=cca4c83aff122fd01e10eb20e0cb8cf6&format=json')
-      .success(function (data) {
-      var artists = _.map(data.artists.artist, function (artist) {
-        var image = _.last(artist.image)['#text'];
+    var params = {
+      method: 'chart.gethypedartists',
+      api_key: API_KEY,
+      format: 'json'
+    };
 
-        return {
-          id: artist.mbid,
-          name: artist.name,
-          image: image
-        };
-      });
-
-      artists = _.filter(artists, function (artist) {
-        return artist.id !== undefined && artist.id.length > 0;
-      });
-
-      deferred.resolve(artists);
+    $http.get(BASE_URL, { params: params }).success(function (data) {
+      deferred.resolve(processResponse(data));
     });
 
     return deferred.promise;
-  };
+  }
+
+  function processResponse(data) {
+    var artists = _.map(data.artists.artist, buildArtist);
+    artists = _.filter(artists, artistShouldHaveId);
+    return artists;
+  }
+
+  function buildArtist(artistData) {
+    var image = _.last(artistData.image)['#text'];
+
+    return {
+      id: artistData.mbid,
+      name: artistData.name,
+      image: image
+    };
+  }
+
+  function artistShouldHaveId(artist) {
+    return artist.id !== undefined && artist.id.length > 0;
+  }
 
   return { getHypedArtists: getHypedArtists };
 });
